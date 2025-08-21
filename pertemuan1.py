4bf8a5a811ba2f5197dea7c81f184093495ef46c
Farhan4456789 <farhanzoera148@gmail.com>
2025-08-21 19:05:06 +0700

    Menambahkan file pertemuan1.py

---
+++ pertemuan1.py
@@ -0,0 +1,31 @@
+x = 1
+y = 1.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1
+y = 3451233412312111
+z = 2131412
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x = 1.4
+y = 2.04
+z = 43.1
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1312.e3
+y = 123e100
+z = 1231E100
+print(type(x))
+print(type(y))
+print(type(z))
