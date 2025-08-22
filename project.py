f12418d00af242ecaea77a807a6cad91b017df53
Farhan4456789 <farhanzoera148@gmail.com>
2025-08-23 00:44:11 +0700

    first commit

---
+++ project.py
@@ -0,0 +1,32 @@
+text = "Hello World"
+print(text[6])
+
+text = 'Hello World'
+print (len(text))
+
+text = 'Hello World'
+print(type(text))
+
+text = 'Hello World'
+print(type(text))
+
+shift = 3
+
+
+text = 'Hello World'
+print(type(text))
+shift = 3
+print(shift)
+
+
+
+
+text = 'Hello World'
+print(type(text))
+shift = 3
+print(type(shift))
+
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
